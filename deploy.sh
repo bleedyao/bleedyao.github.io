154614319ef90a0baad7ad92eef33f00a7f3851a
yaoluhao <logcat001@gmail.com>
2017-05-01 21:16:29 +0800

    完成站内搜索

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ hexo generate
 cp -R public/* .deploy/bleedyao.github.io
 cd .deploy/bleedyao.github.io
 git add .
-git commit -m "add LeanCloud"
+git commit -m "add LeanCloud counter"
 git push origin master
