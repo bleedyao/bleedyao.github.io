e08e3e85dcd214782b8ddbfb4251067349907c3d
yaoluhao <logcat001@gmail.com>
2017-05-01 15:10:45 +0800

    add LeanCloud

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ hexo generate
 cp -R public/* .deploy/bleedyao.github.io
 cd .deploy/bleedyao.github.io
 git add .
-git commit -m "add 404.html"
+git commit -m "add LeanCloud"
 git push origin master
